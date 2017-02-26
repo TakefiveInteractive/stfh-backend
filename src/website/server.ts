5f4957f2efd72a4ce3694d49f68d99bd51d9809b
Yifei Teng <tengyifei88@gmail.com>
2017-02-25 19:40:50 -0600

    temp streaming

--- src/website/server.ts
+++ src/website/server.ts
@@ -2,13 +2,11 @@ import * as express from 'express';
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World');
-});
-
 // HTTPS challenge handling
 app.get('/.well-known/acme-challenge/*', (req, res, next) => {
   res.send(process.env.LETS_ENCRYPT_CHALLENGE);
 });
+
+app.use('/', express.static('public'))
 
 export default app;
