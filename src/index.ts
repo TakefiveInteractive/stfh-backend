4f06dfd31d205f378612e2c5512427ea9306bb45
Yifei Teng <tengyifei88@gmail.com>
2017-02-25 20:58:56 -0600

    fix express handling bug

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-import app from './website/server';
+import { app, httpServer } from './website/server';
 import io from './relay/server';
-import * as http from 'http';
-var ExpressPeerServer = require('peer').ExpressPeerServer;
 
-const httpServer = http.createServer(app);
 io.attach(httpServer);
-
-let server = httpServer.listen(process.env.PORT || 3000);
-
-app.use('/peer', ExpressPeerServer(server, { proxied: true }));
-
-server.on('connection', id => {
-  console.log(`Client ${id} connected`);
-});
-
-server.on('disconnect', id => {
-  console.log(`Client ${id} gone`);
-});
--- src/website/server.ts
+++ src/website/server.ts
@@ -1,12 +1,24 @@
 import * as express from 'express';
+import * as http from 'http';
+const  ExpressPeerServer = require('peer').ExpressPeerServer;
 
-const app = express();
+export const app = express();
 
 // HTTPS challenge handling
 app.get('/.well-known/acme-challenge/*', (req, res, next) => {
   res.send(process.env.LETS_ENCRYPT_CHALLENGE);
 });
 
+export const httpServer = http.createServer(app);
+let server = httpServer.listen(process.env.PORT || 3000);
+
+app.use('/peer', ExpressPeerServer(server, { proxied: true }));
 app.use('/', express.static('public'))
 
-export default app;
+server.on('connection', id => {
+  console.log(`Client ${id} connected`);
+});
+
+server.on('disconnect', id => {
+  console.log(`Client ${id} gone`);
+});
