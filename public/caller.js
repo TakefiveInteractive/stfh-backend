03a30028ec49798d0830df4eab33c3bec6e40e04
Yifei Teng <tengyifei88@gmail.com>
2017-02-25 20:25:04 -0600

    use external peerjs

---
+++ public/caller.js
@@ -0,0 +1,25 @@
+var peer = new Peer({ key: 'ob1bohiqjkedn29', secure: true })
+var video = document.querySelector('video')
+
+peer.on('connection', function (conn) {
+  console.log('On connection', conn)
+})
+
+peer.on('open', function (id) {
+  console.log('My ID ', id)
+
+  var peerId = window.location.search.replace('?', '') || 'electron-video'
+  console.log('peerID', peerId)
+  var conn = peer.connect(peerId)
+})
+
+peer.on('call', function (call) {
+  console.log('on call', call)
+
+  call.answer()
+
+  call.on('stream', function (stream) {
+    video.src = URL.createObjectURL(stream)
+  })
+
+})
