f4b37cb3404bd068dba6128a54cd84c1baf31860
Yifei Teng <tengyifei88@gmail.com>
2017-02-26 12:47:37 -0600

    temp disable website build

--- gulp/gulp-build-website.js
+++ gulp/gulp-build-website.js
@@ -10,14 +10,14 @@ const sourcePath = ['stfh-frontend/dist/**/*'];
 const sourceDest = 'public';
 
 gulp.task('build-website-exec', [], function(cb) {
-  exec('cd stfh-frontend && npm i && npm run build', (err, stdout, stderr) => {
+  /*exec('cd stfh-frontend && npm i && npm run build', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     cb(err);
-  });
+  });*/
 });
 
 gulp.task('build-website', ['build-website-exec'], function() {
-  return gulp.src(sourcePath).pipe(gulp.dest(sourceDest));
+  /*return gulp.src(sourcePath).pipe(gulp.dest(sourceDest));*/
 });
 
