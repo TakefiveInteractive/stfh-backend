988f0616bae6ca195fb59e64060892b06fc6022d
Yifei Teng <tengyifei88@gmail.com>
2017-02-25 18:47:24 -0600

    https challenge

--- src/website/server.ts
+++ src/website/server.ts
@@ -3,7 +3,12 @@ import * as express from 'express';
 const app = express();
 
 app.get('/', (req, res, next) => {
-  res.sendStatus(400);
+  res.send('Hello World');
+});
+
+// HTTPS challenge handling
+app.get('/.well-known/acme-challenge/(.*)', (req, res, next) => {
+  res.send(process.env.LETS_ENCRYPT_CHALLENGE);
 });
 
 export default app;
