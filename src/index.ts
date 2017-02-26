1dfe8286970ceba421950c1a91e7290b0f7e13f7
Yifei Teng <tengyifei88@gmail.com>
2017-02-25 20:41:33 -0600

    use our own peer server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import app from './website/server';
 import io from './relay/server';
 import * as http from 'http';
+var ExpressPeerServer = require('peer').ExpressPeerServer;
 
 const httpServer = http.createServer(app);
 io.attach(httpServer);
 
-httpServer.listen(process.env.PORT || 3000);
+let server = httpServer.listen(process.env.PORT || 3000);
+
+app.use('/peer', ExpressPeerServer(server, { }));
+
+server.on('connection', id => {
+  console.log(`Client ${id} connected`);
+});
+
+server.on('disconnect', id => {
+  console.log(`Client ${id} gone`);
+});
